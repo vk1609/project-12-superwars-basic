ced0b67e15937028fcd5e62b9e3ee02d1373e6e2
unknown <vamsi3353@gmail.com>
2020-07-26 21:11:55 +0530

    VAMSI

--- src/app.js
+++ src/app.js
@@ -26,6 +26,21 @@ const initPlayers = (players) => {
     let detailedPlayers = [];
     // Create players using for loop
     // Type your code here
+    let position = 0,
+        size = players.length;
+    for (position, size; size > 0; size--, position++) {
+        detailedPlayers[position] = {
+            name: players[position],
+            strength: getRandomStrength(),
+            image: "images/super-" + (position + 1) + ".png"
+        };
+
+        if ((position) % 2 == 0) {
+            detailedPlayers[position].type = "hero";
+        } else {
+            detailedPlayers[position].type = "villan";
+        }
+    }
 
     return detailedPlayers;
 }
@@ -34,6 +49,7 @@ const initPlayers = (players) => {
 const getRandomStrength = () => {
     // Return a random integer (0,100]
     // Note: You can use Math.random() and Math.ceil()
+    return Math.random() * 100;
 }
 
 const buildPlayers = (players, type) => {
@@ -42,6 +58,16 @@ const buildPlayers = (players, type) => {
     // Loop through players and accumulate HTML template
     // depending of type of player(hero|villain)
     // Type your code here
+    for (i = 0; i < players.length; i++) {
+        player = `
+            <div class="player">
+                <img src="${players[i].image}" alt="">
+                <div class="name">${players[i].name}</div>
+                <div class="strength">${players[i].strength}</div>
+            </div>`;
+        if (players[i].type == type)
+            fragment = fragment + player;
+    }
 
     return fragment;
 }
